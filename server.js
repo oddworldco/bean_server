038d3dd3a1cb1964118321ec5ac16a2d6e81f234
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-07-09 15:51:21 -0500

    updates header

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 require('dotenv').load();
 
-// var Bean = require('ble-bean');
-// var beanStream = require('ble-bean-stream');
+var Bean = require('ble-bean');
+var beanStream = require('ble-bean-stream');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -17,18 +17,22 @@ var fertility_data, test_db;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 // 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds019746.mlab.com:19746/heroku_8d8nzwb1'
-mongo.connect('mongodb://heroku_1gsdf4dv:99kfcmdds2utedjvtbdkrvuj92@ds151702.mlab.com:51702/heroku_1gsdf4dv').then(function(database){
+mongo.connect('mongodb://heroku_1gsdf4dv:99kfcmdds2utedjvtbdkrvuj92@ds151702.mlab.com:3000/heroku_1gsdf4dv').then(function(database){
 
  db = database;
  // Collections
  test_db = db.collection('beantest'); //TODO: change this to real database after test
   // Start Server
-  app.listen(process.env.PORT || 51702, function() {
+  app.listen(process.env.PORT || 3000, function() {
     console.log('Server: Running on port 51702');
   });
 
@@ -40,12 +44,13 @@ app.post('/collect_data', function (req, res, next) {
   console.log('req: ', req.body);
   let request = req.body,
       timeStamp = request.timeStamp,
+      device = request.device,
       uuid = request.uuid,
       temp = request.temp;
 
 
   // to do: handle new sensortag
-  test_db.update({ 'uuid': uuid }, {'$push': {'data': {'timeStamp': timeStamp, 'temp': temp}}})
+  test_db.update({ 'device': device }, {'$push': {'data': {'timeStamp': timeStamp, 'temp': temp}}})
 
   res.send('Got a POST request');
 })
