2f90526913f1b96719f1b3febecd6f7b09716fd0
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-09-04 17:45:17 -0500

    updates bean structure

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var MongoClient = require('mongodb').MongoClient;
 var mongo = require('mongodb-bluebird');
 
 var collection = 'smartypants_trial1';
-var db, fertility_data, test_db;
+var db, fertility_data, test_db, collection;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,8 +24,11 @@ app.use(function(req, res, next) {
 mongo.connect('mongodb://heroku_1gsdf4dv:99kfcmdds2utedjvtbdkrvuj92@ds151702.mlab.com:51702/heroku_1gsdf4dv').then(function(database){
 
  db = database;
+ collection = 'beantest'
+ //'smartypants_trial1'
+
  // Collections
- test_db = db.collection('smartypants_trial1'); //TODO: change this to real database after test
+ test_db = db.collection(collection); //TODO: change this to real database after test
   // Start Server
   app.listen(process.env.PORT || 3000, function() {
     console.log('Server: Running on port 3000');
@@ -38,13 +41,11 @@ mongo.connect('mongodb://heroku_1gsdf4dv:99kfcmdds2utedjvtbdkrvuj92@ds151702.mla
 app.post('/collect_data', function (req, res, next) {
   console.log('req: ', req.body);
   let request = req.body,
-      timeStamp = request.timeStamp,
-      uuid = request.uuid,
-      temp = request.temp;
-
-  // to do: handle new sensortag
+        data = request.data,
+        timeStamp = request.timeStamp,
+        uuid = request.uuid;
   // test_db.update({ 'uuid': uuid }, {'$push': {'data': {'timeStamp': timeStamp, 'temp': temp}}})
-  test_db.insert({'uuid': uuid, 'timeStamp': timeStamp, 'temp': temp})
+  test_db.insert({'uuid': request.uuid, 'timeStamp': timeStamp, 'data': data})
 
   res.send('Got a POST request. Data sent to mlab collection '+collection);
 })
