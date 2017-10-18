b513bef727075b4b99dfd109b4df3198b79d1626
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-10-17 22:12:31 -0500

    inserts generic data structure

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/collect_data', function (req, res, next) {
         timeStamp = request.timeStamp,
         uuid = request.uuid;
   // test_db.update({ 'uuid': uuid }, {'$push': {'data': {'timeStamp': timeStamp, 'temp': temp}}})
-  test_db.insert({'uuid': request.uuid, 'timeStamp': timeStamp, 'data': data})
+  test_db.insert({'data': data})
 
   res.send('Got a POST request. Data sent to mlab collection '+collection);
 })
