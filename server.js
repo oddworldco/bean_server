5659f2f191b78964e3fcd1e7c873465147501ab5
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-12-02 17:17:57 -0600

    auth

--- server.js
+++ server.js
@@ -18,13 +18,10 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-//DB_USER=heroku_q6lkjqhb
-//DB_PASSWORD=Ly_0zNGxBC2UVjbkbGVZvzsdfObWLUiV
  
 // mongo.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds121192.mlab.com:21192/heroku_q6lkjqhb').then(function(database){
 
-mongo.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds121192.mlab.com:21192/heroku_q6lkjqhb').then(function(database){
+mongo.connect('mongodb://heroku_q6lkjqhb:1g3gogthbor56d5kfp2t3lig43@ds121192.mlab.com:21192/heroku_q6lkjqhb').then(function(database){
 
  db = database;
  //'beantest'
@@ -50,10 +47,12 @@ app.post('/signup', function (req, res, next) {
   prod_db.insert({'data': request})
 
   res.send('Got a POST request. Data sent to mlab collection');
+  return res('success!')
 })
 
 app.get('/test', function (req, res) {
 
   console.log('message!!');
   res.send('Successful request!!');
+  return res('error')
 })
